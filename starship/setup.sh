f72c01520516e81fc4d54cc68afb988fb0bcc0c1
juntaka <junya.takaesu@gmail.com>
2023-04-14 00:30:42 +0900

    Add starship.toml

--- starship/setup.sh
+++ starship/setup.sh
@@ -1,2 +1,4 @@
 # Enable starship prompt 2022-10-29
 [ -x "$(command -v starship)" ] && eval "$(starship init bash)"
+# https://starship.rs/config
+[ -x "$(command -v starship)" ] && export STARSHIP_CONFIG=$DIR_NAME_MY_BASH_ENV/starship/starship.toml
