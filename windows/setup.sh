749cd949c9c4089bc6d643c48161b4293103b26e
juntaka <junya.takaesu@gmail.com>
2023-04-15 19:55:17 +0900

    Append windows PATH for devcontainer command

--- windows/setup.sh
+++ windows/setup.sh
@@ -1,2 +1,3 @@
 [[ -d "/run/WSL" ]] && alias start="explorer.exe"
 [[ -d "/run/WSL" ]] && . $DIR_NAME_MY_BASH_ENV/windows/wsl2/setup.sh
+[[ -d "/run/WSL" ]] && export PATH="$PATH:/mnt/c/Users/ryoku/AppData/Roaming/Code/User/globalStorage/ms-vscode-remote.remote-containers/cli-bin"
