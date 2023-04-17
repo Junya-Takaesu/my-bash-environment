da7f02cf3a273452f5091adecb4493b2071c89f7
Takaesu Junya <takaesu_junya@ca-adv.co.jp>
2023-04-17 11:26:07 +0900

    Make mac setup a little bit more spicy

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,1 +1,6 @@
+! [ $(uname -s) = 'Darwin' ] && return
+
 [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
+
+echo "You are using a nice awesome Mac!🍎"
+# say "You are using a nice awesome Mac!"
