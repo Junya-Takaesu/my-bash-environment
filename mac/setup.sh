469e33869af960d80773f7133bcfaeef3104d614
Takaesu Junya <takaesu_junya@ca-adv.co.jp>
2023-05-16 11:40:05 +0900

    Add alias of start for macOS

--- mac/setup.sh
+++ mac/setup.sh
@@ -2,5 +2,7 @@
 
 [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
 
+alias start="open"
+
 echo "You are using a nice awesome Mac!🍎"
 # say "You are using a nice awesome Mac!"
