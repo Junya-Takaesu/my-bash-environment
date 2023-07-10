302dcaf18f15011d3d3b9ecd2b21817b8bc202f6
juntaka <junya.takaesu@gmail.com>
2023-07-11 01:01:03 +0900

    Support fzf for both MacOS and others

--- sources/fzf/setup.sh
+++ sources/fzf/setup.sh
@@ -1,2 +1,7 @@
 # Fuzzy search cli tool
-[ -f ~/.fzf.bash ] && [ -x "$(command -v fzf)" ] && source ~/.fzf.bash
+# For the case of fzf installed by "brew"
+[ -x "$(command -v fzf)" ] && [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
+# # For the case of fzf installed by "apt"
+[ -x "$(command -v fzf)" ] && [ -f /usr/share/doc/fzf/examples/key-bindings.bash ] && source /usr/share/doc/fzf/examples/key-bindings.bash
+export FZF_DEFAULT_OPTS='--height 100% --border'
