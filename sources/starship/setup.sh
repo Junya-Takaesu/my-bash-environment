4e71320ad43704a4eeddacf97a3dc494b46ed116
juntaka <junya.takaesu@gmail.com>
2023-09-10 14:42:24 +0900

    Add OS detection on the prompt

--- sources/starship/setup.sh
+++ sources/starship/setup.sh
@@ -1,4 +1,4 @@
 # Enable starship prompt 2022-10-29
-[ -x "$(command -v starship)" ] && eval "$(starship init bash)"
+[ -x "$(command -v starship)" ] && eval "$(starship init bash)" && eval "$(starship completions bash)"
 # https://starship.rs/config
 [ -x "$(command -v starship)" ] && export STARSHIP_CONFIG=$DIR_FOR_SRC_SCRIPTS/starship/starship.toml
