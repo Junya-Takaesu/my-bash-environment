cd8c1895f1c114b59a3244b71d29e6acadbd5bf1
Takaesu Junya <takaesu_junya@ca-adv.co.jp>
2023-11-15 15:18:11 +0900

    Update to enable "brew" tab compaletion

--- sources/mac/setup.sh
+++ sources/mac/setup.sh
@@ -1,5 +1,16 @@
 ! [ $(uname -s) = 'Darwin' ] && return
 
-[ -f /usr/local/etc/bash_completion ] && source /usr/local/etc/bash_completion
+# Brew requires this line to initialize brew's environment variables
+# Old installation of brew miss this.
+# Details here: https://github.com/Homebrew/homebrew-core/issues/125461
+eval $(/usr/local/bin/brew shellenv)
+
+# This is a line that loads brew package of bash-completion@1.
+# [ -f /usr/local/etc/bash_completion ] && source /usr/local/etc/bash_completion
+# This is a line that loads brew package of bash-completion@2. Commented because this is going to be loaded in my "brew_completion.sh"
+# [[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
 
 alias start="open"
+
+# enable brew tab completion
+[ -f $(dirname ${BASH_SOURCE[0]})/brew_completion.sh ] && source $(dirname ${BASH_SOURCE[0]})/brew_completion.sh
