6dd45a1cc3e3c121cd5db99ceb0752612d66978d
Takaesu Junya <takaesu_junya@ca-adv.co.jp>
2023-11-15 17:12:16 +0900

    Update fzf loading

--- sources/fzf/setup.sh
+++ sources/fzf/setup.sh
@@ -1,7 +1,15 @@
+! [ -x "$(command -v fzf)" ] && return
+
 # Fuzzy search cli tool
 # For the case of fzf installed by "brew"
-[ -x "$(command -v fzf)" ] && [ -f ~/.fzf.bash ] && source ~/.fzf.bash
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
 
-# # For the case of fzf installed by "apt"
-[ -x "$(command -v fzf)" ] && [ -f /usr/share/doc/fzf/examples/key-bindings.bash ] && source /usr/share/doc/fzf/examples/key-bindings.bash
+# For the case of fzf installed by "apt"
+[ -f /usr/share/doc/fzf/examples/key-bindings.bash ] && source /usr/share/doc/fzf/examples/key-bindings.bash
+
+# Set options for fzf command
 export FZF_DEFAULT_OPTS='--height 100% --border'
+
+# Usage: `some_command **<TAB>` will complete the arguments of `some_command`.
+# See what commands are supported for this fzf feature: `complete | grep _fzf`
+export FZF_COMPLETION_OPTS='--height 100% --layout=default'
