68ab8af0b8b8df6568131baefad0266dd003b8e0
Takaesu Junya <takaesu_junya@ca-adv.co.jp>
2023-12-11 19:35:43 +0900

    Add ntfy

---
+++ sources/ntfy/setup.sh
@@ -0,0 +1,2 @@
+# Define NTFY_TOPIC in sources/custom-hooks/.hooks/ntfy-topic.sh(Do not leak the NTFY_TOPIC to the public, otherwise the TOPIC will be abused!!)
+[ -n "$NTFY_TOPIC" ] && alias ntfyme="curl -d \"Command Done\" ntfy.sh/$NTFY_TOPIC"
