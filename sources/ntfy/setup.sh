3a56ccc801c598c27e0b20953dc2c75dceaa500e
Takaesu Junya <takaesu_junya@ca-adv.co.jp>
2023-12-13 14:08:23 +0900

    Remove json output of ntfy

--- sources/ntfy/setup.sh
+++ sources/ntfy/setup.sh
@@ -1,2 +1,2 @@
 # Define NTFY_TOPIC in sources/custom-hooks/.hooks/ntfy-topic.sh(Do not leak the NTFY_TOPIC to the public, otherwise the TOPIC will be abused!!)
-[ -n "$NTFY_TOPIC" ] && alias ntfyme="curl -d \"Command Done\" ntfy.sh/$NTFY_TOPIC"
+[ -n "$NTFY_TOPIC" ] && alias ntfyme="curl --silent -d \"Command Done\" ntfy.sh/$NTFY_TOPIC > /dev/null"
