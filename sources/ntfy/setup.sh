4c5c97200c8503c8854e0028e2fedf817300e446
Takaesu Junya <takaesu_junya@ca-adv.co.jp>
2023-12-22 14:20:23 +0900

    Add slack notification

--- sources/ntfy/setup.sh
+++ sources/ntfy/setup.sh
@@ -1,2 +1,7 @@
-# Define NTFY_TOPIC in sources/custom-hooks/.hooks/ntfy-topic.sh(Do not leak the NTFY_TOPIC to the public, otherwise the TOPIC will be abused!!)
-[ -n "$NTFY_TOPIC" ] && alias ntfyme="curl --silent -d \"Command Done\" ntfy.sh/$NTFY_TOPIC > /dev/null"
+# Define NTFY_TOPIC in secrets.sh(Do not leak the NTFY_TOPIC to the public, otherwise the TOPIC will be abused!!)
+! [ -n "$NTFY_TOPIC" ] && return
+# Define SLACK_WEBHOOK_URL in secrets.sh(Do not leak the SLACK_WEBHOOK_URL to the public, otherwise the SLACK_WEBHOOK_URL will be abused!!)
+! [ -n "$SLACK_WEBHOOK_URL" ] && return
+
+JSON_PAYLOAD='{\"username\": \"ntfy\", \"text\": \"Command Done\", \"icon_emoji\": \":i-got-notification:\" }'
+alias ntfyme="curl --silent -d \"Command Done\" ntfy.sh/$NTFY_TOPIC > /dev/null; curl --silent -X POST --data-urlencode \"payload=$JSON_PAYLOAD\" $SLACK_WEBHOOK_URL > /dev/null"
