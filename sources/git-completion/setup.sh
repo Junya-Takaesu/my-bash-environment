00be38a7f2d9b9799f5f113d13fad49d49bfb692
Junya Takaesu <takaesu_junya@ca-adv.co.jp>
2025-08-24 08:04:12 +0900

    Update to use $HOME var to load .git-completion

--- sources/git-completion/setup.sh
+++ sources/git-completion/setup.sh
@@ -1,3 +1,4 @@
 # source symlink for Macos
 # .git-completion.bash -> /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash
-source .git-completion.bash
+# source .git-completion.bash # relative path does not work in all directories
+source $HOME/.git-completion.bash
