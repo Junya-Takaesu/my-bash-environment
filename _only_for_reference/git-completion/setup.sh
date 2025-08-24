e6a2a8995cf5319caf31713ee09293d319e0d4be
juntaka <junya.takaesu@gmail.com>
2025-08-24 09:21:51 +0900

    disable loading of git completion

---
+++ _only_for_reference/git-completion/setup.sh
@@ -0,0 +1,11 @@
+# NOTE:
+#     Git tab completion is enabled by bash completion script, so loading it by
+#     youself is not necessary. Installation of bash-completion is recommended.
+#     This script is provided for reference.
+# References:
+# https://git-scm.com/book/en/v2/Appendix-A:-Git-in-Other-Environments-Git-in-Bash
+
+# source symlink for Macos
+# .git-completion.bash -> /Library/Developer/CommandLineTools/usr/share/git-core/git-completion.bash
+# source .git-completion.bash # relative path does not work in all directories
+# source $HOME/.git-completion.bash
