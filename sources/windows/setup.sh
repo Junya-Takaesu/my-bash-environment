204ef9a9066e3da98096e154585534212169c3eb
juntaka <junya.takaesu@gmail.com>
2025-08-31 09:23:43 +0900

    Restore Windows/WSL Specific settings file

---
+++ sources/windows/setup.sh
@@ -0,0 +1,3 @@
+[[ -d "/run/WSL" ]] && alias start="explorer.exe"
+[[ -d "/run/WSL" ]] && source $DIR_FOR_SRC_SCRIPTS/windows/wsl2/setup.sh
+# [[ -d "/run/WSL" ]] && export PATH="$PATH:/mnt/c/Users/ryoku/AppData/Roaming/Code/User/globalStorage/ms-vscode-remote.remote-containers/cli-bin"
--- sources/windows/wsl2/setup.sh
+++ sources/windows/wsl2/setup.sh
@@ -1,4 +1,4 @@
 # Enable duplicate tabs/panes by telling the bash the current wslpath in Windows path format
 # Details:
 #   https://learn.microsoft.com/en-us/windows/terminal/tutorials/new-tab-same-directory
-PROMPT_COMMAND=${PROMPT_COMMAND:+"$PROMPT_COMMAND; "}'printf "\e]9;9;%s\e\\" "$(wslpath -w "$PWD")"'
+# PROMPT_COMMAND=${PROMPT_COMMAND:+"$PROMPT_COMMAND; "}'printf "\e]9;9;%s\e\\" "$(wslpath -w "$PWD")"'
